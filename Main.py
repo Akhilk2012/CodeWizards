9026bc4723fc68428ae6716149fa102ed641a553
Akhilk2012 <157044908+Akhilk2012@users.noreply.github.com>
2025-02-14 14:28:42 +0530

    New_screen.py

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import pygame , sys
 import os
 
 pygame.init()
+pygame.font.init()
 
 clock = pygame.time.Clock()
 fps = 60
@@ -22,6 +23,10 @@ font = pygame.font.SysFont('Arial', 20)
 font_sub = pygame.font.SysFont('Arial',27,bold= True)
 font_1 = pygame.font.Font("freesansbold.ttf" , 40)
 font_2 = pygame.font.Font("freesansbold.ttf" , 20)
+font_counter = pygame.font.Font("freesansbold.ttf" , 28 )
+font_X = 446
+font_Y = 10
+points = 0
 screen_width = 640
 screen_height = 640
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -33,11 +38,16 @@ pygame.display.set_icon(diplay_icon)
 
 exit_img = pygame.image.load("Exit_button.png").convert_alpha()
 struct_img = pygame.image.load("Struct_button.png").convert_alpha()
+start_button = pygame.image.load("Start_button.png").convert_alpha()
 
 def draw_text(text, x, y, color,font):
     use = font 
     screen.blit(use.render(text, True, color), (x, y))
     
+def counter(x,y,font):
+    count = font
+    point_counter = count.render(f"Points : {points}" , True , ("#879CEB"))
+    screen.blit(point_counter, (x,y))
 
 class Button():
     def __init__(self,x,y,image,scale) :
@@ -63,9 +73,34 @@ class Button():
         screen.blit(self.image, (self.rect.x , self.rect.y))
 
         return action
+class Text_box():
+    def __init__(self,x,y,w,h):
+        self.rect = pygame.Rect(x,y,w,h)
+        self.text = ""
+        self.active = False 
 
 exit_button = Button( 525, 0 , exit_img , 0.25)
+exit_button_1 = Button( 525, 0 , exit_img , 0.25)
 struct_button = Button( 0, 0 , struct_img, 0.15)
+start_button = Button( 225 , 290 , start_button , 0.30)
+
+def main_screen():
+    while True:
+         while True:
+            screen.fill("#1A7499")
+        
+            # Event handling
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if exit_button_1.draw() == True:
+                        return  # Exit struct_screen() and go back to main screen by return
+    
+            exit_button_1.draw() # Second exit button for second screen 
+
+            pygame.display.update()
 def struct_screen():
     while True:
          while True:
@@ -78,9 +113,9 @@ def struct_screen():
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if exit_button.draw() == True:
-                        return  # Exit struct_screen() and go back to main screen
+                        return  # Exit struct_screen() and go back to main screen by return
 
-            # Draw exit button
+            # Draw text and exite button on the instruction menu
             draw_text("Instructions", 250, 30, white,font_sub)
             draw_text("Our software is a task manager that helps in gathering and ", 30, 80, white,font)
             draw_text( "recording tasks we need to complete.", 30, 100, white,font)
@@ -97,14 +132,18 @@ def struct_screen():
 while run :
     clock.tick(fps)
     screen.fill(bg_color)
+    counter(font_X, font_Y, font_counter)
     struct_button.draw()
+    start_button.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if struct_button.draw() == True :
                 struct_screen()
-    draw_text("Task Manager", 200, 100, black , font_1)
+            if start_button.draw() == True :
+                main_screen()
+    draw_text("Task Manager", 185.67, 100, black , font_1)
     pygame.display.flip()
     pygame.display.update()
 
