d6725ededc1e427645a61ada5e263dc42c6ca0e8
Akhilk2012 <157044908+Akhilk2012@users.noreply.github.com>
2025-02-14 11:38:34 +0530

    Exit.py

--- Main.py
+++ Main.py
@@ -14,6 +14,8 @@ red = (255,0,0)
 blue = (0,255,0)
 green = (0,0,255)
 
+bg_color = (135,206,235)
+
 font = pygame.font.SysFont('Arial', 20)
 
 screen_width = 640
@@ -25,23 +27,43 @@ pygame.display.set_caption("Task Manager")
 diplay_icon = pygame.image.load("Windows_icon.png").convert_alpha()
 pygame.display.set_icon(diplay_icon)
 
-exit_button = pygame.image.load("Exit_button.png").convert_alpha()
+exit_img = pygame.image.load("Exit_button.png").convert_alpha()
 
 def draw_text(text, x, y, color=black):
     screen.blit(font.render(text, True, color), (x, y))
 
 class Button():
-    def __init__(self,x,y,image) :
-        self.image = image 
+    def __init__(self,x,y,image,scale) :
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image,(int(width * scale),int(height * scale)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
+        self.clicked = False
     def draw(self):
+        action = False
+        # Getting the mouse position
+        pos = pygame.mouse.get_pos()
+        # Checking if the mouse is over the button
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True 
+                action = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        # This will draw the button on the screen
         screen.blit(self.image, (self.rect.x , self.rect.y))
 
-exit_button = Button(100 , 200 , exit_button)
+        return action
+
+exit_button = Button( 525, 0 , exit_img , 0.25)
 
 while run :
-    screen.fill(white)
+    screen.fill(bg_color)
+
+    if exit_button.draw() == True :
+        run = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
